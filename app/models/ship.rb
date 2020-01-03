ac8029dab6d3a63e46996b609d7962626a18529b
Brandon Weaver <a7xncob@gmail.com>
2020-01-03 00:10:15 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+  
+  attr_accessor :name, :type, :booty
+  
+  @@all = []
+  
+  def initialize(name, type, booty)
+    @name = name
+    @type = type
+    @booty = booty
+    @@all << self
+  end
+  
+  def self.all
+    return @@all
+  end
+  
+  def self.clear
+    @@all = []
+  end
+  
 end
