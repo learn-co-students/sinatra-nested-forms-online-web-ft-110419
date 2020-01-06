cd9e2f2046f859f38a106b18409435e95d8034bc
ncaudill27 <nelson.a.caudill@gmail.com>
2020-01-06 16:37:53 -0500

    Done.

--- app.rb
+++ app.rb
@@ -7,19 +7,25 @@ module FormsLab
     get '/' do
       erb :root
     end
-
+  
     get '/new' do
       erb :'pirates/new'
     end
-    
+  
     post '/pirates' do
-     @p = Pirate.new(params[:pirate])
+     @pirate = Pirate.new(params[:pirate])
 
      params[:pirate][:ships].each do |ship|
       Ship.new(ship)
      end
-     @s1 = Ship.all.first
-     @s2 = Ship.all.last
+     @ship_name_1 = Ship.all.first.name
+     @ship_type_1 = Ship.all.first.type
+     @ship_booty_1 = Ship.all.first.booty
+     
+     @ship_name_2 = Ship.all.last.name
+     @ship_type_2 = Ship.all.last.type
+     @ship_booty_2 = Ship.all.last.booty
+
      
       erb :'pirates/show'
     end
