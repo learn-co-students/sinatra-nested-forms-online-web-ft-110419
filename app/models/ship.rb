da7ff6cdafb488e0dfec94120d0c9c8c013b5441
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-01-06 22:27:02 -0500

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+
+    @@all << self
+  end
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
 end
